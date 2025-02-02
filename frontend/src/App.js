ff06fb6e1e876bc4c9b2fd92ffe03005f21ff57b
kikun1118 <sl9208005963@gmail.com>
2025-02-02 23:06:39 +0900

    修正: frontend をサブモジュールではなく通常フォルダとして追加

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Calculator from "./component/Calculator";
+
+function App() {
+    return (
+        <div>
+            <h1>React 電卓アプリ</h1>
+            <Calculator />
+        </div>
+    );
+}
+
+export default App;
